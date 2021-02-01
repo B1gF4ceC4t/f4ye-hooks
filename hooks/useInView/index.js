e73343ea3e1d3f61f6209d192aff6fb2de54da0c
b1gf4cec4t <a18502641491@gmail.com>
2021-02-01 19:51:01 +0800

    feat(DOM): :sparkles: useInView

---
+++ hooks/useInView/index.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import 'intersection-observer';
+import { getTargetElement } from '../utils';
+
+const isInView = el => {
+  if (!el) return undefined;
+
+  const viewWith =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  const viewHeight =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+
+  const rect = el.getBoundingClientRect();
+
+  if (rect) {
+    const { top, bottom, left, right } = rect;
+    return (
+      bottom > 0 && top <= viewPortHeight && left <= viewPortWidth && right > 0
+    );
+  }
+
+  return false;
+};
+
+const useInView = target => {
+  const [inView, setInView] = useState(() =>
+    isInView(getTargetElement(target))
+  );
+
+  useEffect(() => {
+    const el = getTargetElement(target);
+    if (!el) {
+      return () => {};
+    }
+
+    const intersectionObserver = new IntersectionObserver(entries => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          setInViewport(true);
+        } else {
+          setInViewport(false);
+        }
+      }
+    });
+
+    intersectionObserver.observe(el);
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, [target]);
+
+  return inView;
+};
+
+export default useInView;
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    'hooks/**/*.js',
+    '!hooks/**/__test__/*.test.js',
+    '!hooks/**/demo/*.js'
+  ],
+  setupFiles: ['<rootDir>/enzyme.setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
 };
